516c951f6897d27c516a852f054bc474f82c8486
Argenis Oldair Palacios Escobar A.K.A. Dr. Lfnt <Dr.Lfnt@gmail.com>
2017-05-20 00:16:54 -0500

    listo ajuste
    ajuste a reserva de las ultmas actualizaciones

--- send.php
+++ send.php
@@ -1,15 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Contacto Profiles</title> <!-- Aquí va el título de la página -->
+
+</head>
+
+<body>
 <?php
-if(isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["comentario"]) ){
-$to = "contacto@profiles.com.mx";
-$subject = "Mensaje Enviado";
-$contenido .= "Nombre: ".$_POST["nombre"]."\n";
-$contenido .= "Email: ".$_POST["email"]."\n\n";
-$contenido .= "Comentario: ".$_POST["comentario"]."\n\n";
-$header = "From: contacto@profiles.com.mx\nReply-To:".$_POST["email"]."\n";
-$header .= "Mime-Version: 1.0\n";
-$header .= "Content-Type: text/plain";
-if(mail($to, $subject, $contenido ,$header)){
-echo "Mail Enviado.";
+
+
+$Nombre = $_POST['nombre'];
+$Correo = $_POST['email'];
+$Asunto = $_POST['asunto'];
+$Mensaje = $_POST['comentario'];
+
+if ($Nombre=='' || $Correo=='' || $Mensaje==''){
+
+echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
+
+}else{
+
+
+    require("includes/class.phpmailer.php");
+    $mail = new PHPMailer();
+
+    $mail->From     = "enviar@profiles.com.mx";   
+    $mail->FromName = $Nombre; 
+    $mail->AddAddress("contacto@profiles.com.mx");
+
+
+    $mail->WordWrap = 50; 
+    $mail->IsHTML(true);     
+    $mail->Subject  =  "Contacto";
+    $mail->Body     =  "Nombre: $Nombre \n<br />".
+    "Email: $Correo \n<br />".
+    "Asunto: $Asunto \n<br />".
+    "Mensaje: $Mensaje \n<br />";
+
+
+
+    $mail->IsSMTP(); 
+    $mail->Host = "mail.profiles.com.mx";  
+    $mail->SMTPAuth = true; 
+    $mail->Username = "enviar@profiles.com.mx";  
+    $mail->Password = "GB7sTG7QMmG("; 
+
+    if ($mail->Send())
+        
+    echo "<script>location.href ='javascript:history.back()';</script>";
+    else
+    echo "<script>location.href ='javascript:history.back()';</script>";
+
 }
-}
+
 ?>
+</body>
+</html>
